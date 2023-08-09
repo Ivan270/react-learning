e85756eb33ee7b0c634d33c81438f98efa2faaff
Ivan Verdugo <ivan270@gmail.com>
2023-08-09 12:34:12 -0400

    se agrega componente ExpenseDate y su estilo

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,14 @@
+import './ExpenseDate.css';
+export default function ExpenseDate(props) {
+	const month = props.date.toLocaleDateString('en-US', { month: 'short' });
+	const year = props.date.toLocaleDateString('en-US', { year: 'numeric' });
+	const day = props.date.toLocaleDateString('en-US', { day: '2-digit' });
+
+	return (
+		<div className="expense-date">
+			<div className="expense-date__month">{month}</div>
+			<div className="expense-date__year">{year}</div>
+			<div expense-date="expense-date__day">{day}</div>
+		</div>
+	);
+}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-	const month = props.date.toLocaleDateString('en-US', { month: 'short' });
-	const year = props.date.toLocaleDateString('en-US', { year: 'numeric' });
-	const day = props.date.toLocaleDateString('en-US', { day: '2-digit' });
-
 	return (
 		<div className="expense-item">
-			<div>
-				<div>{month}</div>
-				<div>{year}</div>
-				<div>{day}</div>
-			</div>
+			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
