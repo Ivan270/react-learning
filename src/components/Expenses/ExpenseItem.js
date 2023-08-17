96678c48dd3afb6ac083ef0f66d840aaa254da02
Ivan Verdugo <ivan270@gmail.com>
2023-08-17 12:20:50 -0400

    using useState, implemented toggle for title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+	// useState(initialValue), using array destructuring
+	const oldVal = props.title;
+	const newVal = 'Cambio!';
+	const [title, setTitle] = useState(oldVal);
+
 	const clickHandler = () => {
-		console.log('clicked!!');
+		if (title === oldVal) {
+			setTitle(newVal);
+		} else {
+			setTitle(oldVal);
+		}
 	};
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
 			<button onClick={clickHandler}>Change Title</button>
