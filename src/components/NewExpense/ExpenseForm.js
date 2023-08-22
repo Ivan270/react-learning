d064c358957bc0e79bf9658084de6fec030d077a
Ivan Verdugo <ivan270@gmail.com>
2023-08-22 16:14:40 -0400

    added alternative for input change handler, single function for every input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,12 +43,27 @@ const ExpenseForm = () => {
 		// });
 	};
 
+	const inputChangeHandler = (identifier, value) => {
+		if (identifier === 'title') {
+			setEnteredTitle(value);
+		} else if (identifier === 'amount') {
+			setEnteredAmount(value);
+		} else {
+			setEnteredDate(value);
+		}
+	};
+
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						onChange={(event) =>
+							inputChangeHandler('title', event.target.value)
+						}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
@@ -56,7 +71,9 @@ const ExpenseForm = () => {
 						type="number"
 						min="0.01"
 						step="0.01"
-						onChange={amountChangeHandler}
+						onChange={(event) =>
+							inputChangeHandler('amount', event.target.value)
+						}
 					/>
 				</div>
 				<div className="new-expense__control">
@@ -65,7 +82,7 @@ const ExpenseForm = () => {
 						type="date"
 						min="2019-01-01"
 						max="2022-12-31"
-						onChange={dateChangeHandler}
+						onChange={(event) => inputChangeHandler('date', event.target.value)}
 					/>
 				</div>
 			</div>
