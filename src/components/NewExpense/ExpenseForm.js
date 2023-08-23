686a436246ce448b41c0194af894ee31695cf386
Ivan Verdugo <ivan270@gmail.com>
2023-08-23 10:51:44 -0400

    added child to parent component communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	// valor inicial es cadena de texto vacía
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
@@ -60,7 +60,7 @@ const ExpenseForm = () => {
 			amount: enteredAmount,
 			date: new Date(enteredDate),
 		};
-		console.log(expenseData);
+		props.onSaveExpenseData(expenseData);
 		// Clearing form . . .
 		setEnteredAmount('');
 		setEnteredDate('');
